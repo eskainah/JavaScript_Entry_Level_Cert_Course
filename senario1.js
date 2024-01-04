d7183a6e9df030def4bedefd1ff827104f522da3
eskainah <eskainah@gmail.com>
2024-01-04 19:50:41 +0200

    Code for 2.0.15  LAB  Variables

---
+++ senario1.js
@@ -0,0 +1,15 @@
+
+let firstPersonName = "Maxwell Wright";
+let firstPersonPhone = "(0191) 7196495";
+let firstPersonEmail = "Curabitur.egestas.nunc@nonummyac.co.uk";
+console.log(firstPersonName, firstPersonPhone, firstPersonEmail);
+
+let secondPersonName = "Raja Villarreal";
+let secondPersonPhone = "0866 398 2895";
+let secondPersonEmail = "posuere.vulputate@sed.com";
+console.log(secondPersonName, secondPersonPhone, secondPersonEmail);
+
+let thirdPersonName = "Helen Richards";
+let thirdPersonPhone = "0800 1111";
+let thirdPersonEmail = "libero@convallis.edu";
+console.log(thirdPersonName, thirdPersonPhone, thirdPersonEmail);
