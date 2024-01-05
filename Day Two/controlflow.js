a7a2d4b0c3dcd70806e5837370dca937aab2bd6d
eskainah <eskainah@gmail.com>
2024-01-05 22:16:12 +0200

    Logical operations

--- Day Two/controlflow.js
+++ Day Two/controlflow.js
@@ -32,7 +32,7 @@ else
 /**
  * Simple if/elseif/else statement
  */
-let hour = 6;
+/* let hour = 6;
 let greetings;
 
 if (hour<10){
@@ -52,4 +52,33 @@ else{
 
     document.write(greetings)
 }
+ */
+
+/**
+ * Logical Operators 
+ **/
+/* let hour = 9
+if (hour < 10 || hour > 18){
+    document.write("The Office is Close");
+}
+ */
+
+let hour = 9
+let isWeekend = true;
+if (hour < 10 || hour > 18 || isWeekend){
+    document.write("The Office is Close <br/>");
+}
+
+document.write(undefined|| null||0, "<br/>");
+
+//cheack which variable has a value assignment 
+let firstName = "";
+let lastName ="";
+let nickName = "SuperCoder";
+document.write(firstName || lastName|| nickName || "Anonymous");
+
+//short-circuit
+
+true || document.write("not printed");
+false || document.write("<br/>printed");
 
