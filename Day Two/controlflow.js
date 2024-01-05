cb0c38456ce068053dbd37d2e1cdc6ea02a08f01
eskainah <eskainah@gmail.com>
2024-01-05 21:06:35 +0200

    simple if/elseif/else condition

--- Day Two/controlflow.js
+++ Day Two/controlflow.js
@@ -14,7 +14,7 @@ if (hour<18){
 /**
  * Simple if/else statement
  */
-let hour = 20;
+/* let hour = 20;
 let greetings;
 
 if (hour<18){
@@ -24,6 +24,30 @@ if (hour<18){
 }
 else
 {
+    greetings="Good Evening";
+
+    document.write(greetings)
+} */
+
+/**
+ * Simple if/elseif/else statement
+ */
+let hour = 6;
+let greetings;
+
+if (hour<10){
+    greetings = "Good morning";
+
+    document.write(greetings);
+}
+else if(hour < 20)
+{
+    greetings="Good Day";
+
+    document.write(greetings)
+}
+else{
+    
     greetings="Good Evening";
 
     document.write(greetings)
