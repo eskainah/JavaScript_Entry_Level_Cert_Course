3bfaa1a45a55c64199fe2a54f8f48ece8f19b8e7
eskainah <eskainah@gmail.com>
2024-01-05 23:20:57 +0200

    Nested If Created

--- Day Two/controlflow.js
+++ Day Two/controlflow.js
@@ -84,11 +84,59 @@ false || document.write("<br/>printed"); */
 
 
 //&& Operator 
-let hour = 12
+/* let hour = 12
 let minute = 30;
 if (hour ==12 && minute ==30){
     document.write("The Office is Close for lunch break<br/>");
 
 }
 
-document.write(1 && 2 && 3);
+document.write(1 && 2 && 3); */
+
+
+
+let age = 15;
+
+if(age >=14 && age <=90){
+    document.write(`Your age is ${age}`);
+}
+age = 10
+if (!(age >=14 && age <=90))
+{
+    document.write(`<br/>Please try again your age is ${age}`);
+}
+/***
+ * Write the code which asks for a login with prompt.
+
+If the visitor enters "Admin", then prompt for a password, if the input is an empty line or Esc – show “Canceled”, if it’s another string – then show “I don’t know you”.
+
+The password is checked as follows:
+
+If it equals “TheMaster”, then show “Welcome!”,
+Another string – show “Wrong password”,
+For an empty string or cancelled input, show “Canceled”
+The schema (Image 001)
+ */
+let username = prompt("Who is there?");
+
+if (username === "Admin"){
+    let password = prompt("Please enter your Password");
+    if (password === "TheMaster"){
+        alert("Welcome!")
+    }
+    else if (password === "")
+    {
+        alert("No Password Enter");
+    }
+    else
+    {
+        alert("Wrong Password");
+    }
+}
+else if(username === "")
+{
+    alert("No Username Enter")
+}
+else{
+    alert("Wrong Username");
+}
