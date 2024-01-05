b732c5c082c769753f3f726960e48c0515f6502d
eskainah <eskainah@gmail.com>
2024-01-05 13:46:21 +0200

    Adding Line breaks

--- LAB1_Variables.js
+++ LAB1_Variables.js
@@ -7,10 +7,13 @@ console.log(firstPersonName, firstPersonPhone, firstPersonEmail);
 let secondPersonName = "Raja Villarreal";
 let secondPersonPhone = "0866 398 2895";
 let secondPersonEmail = "posuere.vulputate@sed.com";
-console.log(secondPersonName, secondPersonPhone, secondPersonEmail);
+document.write(secondPersonName, secondPersonPhone, secondPersonEmail, "<br/>");
 
 let thirdPersonName = "Helen Richards";
 let thirdPersonPhone = "0800 1111";
 let thirdPersonEmail = "libero@convallis.edu";
 //The below section is calling a varible
 document.write(`${thirdPersonName} ${thirdPersonPhone} ${thirdPersonEmail}`);
+
+let addLineBreak = "Hello\nNew Line";
+console.log(`${addLineBreak}`)
