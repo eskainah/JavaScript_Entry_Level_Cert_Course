5361ddee02d203a4ea974575ca94452d60529ac1
eskainah <eskainah@gmail.com>
2024-01-23 11:27:53 +0200

    Quiz solution added

---
+++ Ice Cream/icecream.js
@@ -0,0 +1,7 @@
+let flavor = "vanilla";
+let vessel = "cone"; 
+let toppings = "peanuts";
+
+if ((flavor === "vanilla" || flavor ==="chocolate") && (vessel ==="bowl" || vessel ==="cone") && (toppings === "sprinkles" || toppings === "peanuts")){
+    console.log("I'd like two scoops of "+ flavor +" ice cream in a "+vessel+" with "+toppings+".")
+}
