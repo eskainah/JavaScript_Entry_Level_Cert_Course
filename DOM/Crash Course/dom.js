f189622714ea47d16532b380243d3e2f37b0aed4
eskainah <eskainah@gmail.com>
2024-02-05 00:17:09 +0200

    set background using Javascript

---
+++ DOM/Crash Course/dom.js
@@ -0,0 +1,12 @@
+//EXAMINE THE DOCUMENT OBJECT
+
+//console.dir(document)
+ 
+//set background color for a class
+let items =  document.querySelectorAll('.list-group-item');
+
+console.log(items)
+
+ for(let i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = 'green';
+} 
