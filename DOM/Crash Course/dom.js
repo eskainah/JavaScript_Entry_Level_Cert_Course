237683fd441f2c673e93ac821e42e5019c6adce2
eskainah <eskainah@gmail.com>
2024-02-05 01:15:48 +0200

    completed

--- DOM/Crash Course/dom.js
+++ DOM/Crash Course/dom.js
@@ -10,3 +10,8 @@ console.log(items)
  for(let i = 0; i < items.length; i++){
     items[i].style.backgroundColor = 'green';
 } 
+
+let itemList = document.querySelector('#items');
+
+
+console.log(itemList.firstChild)
